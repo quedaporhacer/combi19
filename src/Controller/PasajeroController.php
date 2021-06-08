e9d16d6332ccd15f1c9b10ad325b38c2a71575ff
santiago <quedaporhacer.s@gmail.com>
2021-06-08 19:49:34 -0300

    profile

--- src/Controller/PasajeroController.php
+++ src/Controller/PasajeroController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+
 /**
  * @Route("/pasajero")
  */
@@ -68,6 +69,8 @@ class PasajeroController extends AbstractController
      */
     public function show(Pasajero $pasajero): Response
     {
+        
+        
         return $this->render('pasajero/show.html.twig', [
             'pasajero' => $pasajero,
         ]);
