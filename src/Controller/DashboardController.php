de12bec2ffe4c0bd4b1974cabe93a202409b9f6c
santiago <quedaporhacer.s@gmail.com>
2021-06-10 00:49:54 -0300

    Dashboard vista

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ComentarioRepository;
+
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(): Response
+    public function index(ComentarioRepository $comentarioRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'comentarios' => $comentarioRepository->findAll(),
         ]);
     }
 }
